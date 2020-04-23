41851e97c6032497579770a703b1e932e19b8841
joedatlive <joed@live.com>
2020-04-22 19:44:10 -0700

    Added a streams UI experience and re-arranged the config and data files and updated paths.

--- src/main/java/streams/FilterManager.java
+++ src/main/java/streams/FilterManager.java
@@ -13,19 +13,19 @@ public class FilterManager {
             System.out.println("****Reading filters");
             //Read filters.json and instantiate a filter object
             Gson filterjson = new Gson();
-            String filterfile = "C:\\Users\\joed\\Documents\\Source\\secdur\\streamui\\src\\web\\filters.json";
+            String filterfile = "src/web/data/config/filters.json";
             BufferedReader fr = new BufferedReader(
 					new FileReader(filterfile));
             Filter f = filterjson.fromJson(fr, Filter.class);
 
             //Default other filter params.  These should be defaults that the user could override.
-            f.alertSink = "data\\results\\alerts\\results.txt";
-            f.eventSink = "data\\results\\events\\results.txt";
+            f.alertSink = "data/results/alerts/results.txt";
+            f.eventSink = "data/results/events/results.txt";
             f.parallelism = 1;
             
             //And for now, until we build some streams, we need to provide the data
             System.out.println("****Using samplestream.txt");
-            BufferedReader sr = new BufferedReader(new FileReader("data\\config\\samplestream.txt"));
+            BufferedReader sr = new BufferedReader(new FileReader("src/web/data/sample_sources/pwdstream.txt"));
             String str;
             List<String> list = new ArrayList<String>();
             while((str = sr.readLine()) != null){
